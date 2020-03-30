749028fda13d707830fb0ffa8f9c135dcb8dd2d0
luckycharm313 <webmobilepro55@aol.com>
2020-03-30 03:06:58 -0600

    remove project

--- config/common.js
+++
@@ -1,9 +0,0 @@
-exports.send = (res, code, payload, message) => {
-  var response = {
-      code,
-      payload,
-      message
-  };
-
-  return res.send(response);
-}
--- scripts/routes/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var middleware = require('./middleware');
-
-var customer = require('../controllers/customer');
-router.post('/customer/request', customer.sendRequest);
-
-module.exports = router;
